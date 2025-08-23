0f502c17b56f2c4db82fbffbf9b72ff3b7e1b129
Jackson <boyjackson925@gmail.com>
2025-08-23 20:32:32 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,9 +6,8 @@ const cors = require('cors');
 const path = require('path');
 const fs = require('fs');
 
-// -------------------------------
+
 // Config
-// -------------------------------
 const app = express();
 const port = process.env.PORT || 3001;
 
